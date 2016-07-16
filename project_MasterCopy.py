08c397a850b2b87d089040ffbeb49ccaf4f8fe86
Gameexpert <gameexpert13@gmail.com>
2016-07-16 09:26:38 -0500

    List Demo
    List Demo

--- project_MasterCopy.py
+++ project_MasterCopy.py
@@ -1,1 +1,12 @@
 import noise
+
+noiseList = []
+noiseList2D = []
+for x in range(10):
+    for y in range(10):
+        newNoise = noise.snoise2(x,y,1,0.5,2.0)
+        noiseList.append(newNoise)
+    noiseList2D.append(noiseList)
+
+    
+print(noiseList2D)
