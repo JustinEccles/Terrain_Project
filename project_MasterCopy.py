cbe9e113d22d224d524da809b38ccffdd181761b
artaficialAI <tpieschala17@students.polytechnic.org>
2016-07-18 09:53:50 -0400

    Included Optional Args On Noise Gen
    Included optional resolution=10 octaves=1 persistence=0.5 &
    lacunarity=2.0

--- project_MasterCopy.py
+++ project_MasterCopy.py
@@ -1,14 +1,23 @@
 import noise
-size = 10
-noiseList2D = []
-for pos in range(size):
-    noiseList2D.append([])
-x,y = 0,0
-for x in range(size):
-    for y in range(size):
-        newNoise = noise.snoise2(x/size,y/size,1,0.5,2.0)
-        noiseList2D[x].append(newNoise)
+class noise_Gen2D:
+    def __init__(self,scale,resolution=10,octaves=1,persistence=0.5,lacunarity=2.0):
+        self.scale = scale
+        self.resolution = resolution
+        self.noiseList2D = []
+        for pos in range(scale*resolution):
+            self.noiseList2D.append([])
+        self.octaves = octaves
+        self.persistence = persistence
+        self.lacunarity = lacunarity
+        
+    def generate2DNoiseArray(self):
+        for x in range(self.scale*self.resolution):
+            for y in range(self.scale*self.resolution):
+                newNoise = noise.snoise2(x/(self.scale*self.resolution),y/(self.scale*self.resolution),self.octaves,self.persistence,self.lacunarity)
+                self.noiseList2D[x].append(newNoise)
+        return self.noiseList2D
     
-
-for pos in  noiseList2D:
-    print(pos)
+Alpha = noise_Gen2D(1,10,3)
+Beta = Alpha.generate2DNoiseArray()
+for pos in range(Alpha.scale*Alpha.resolution):
+    print(Beta[pos])
