5623a5332146e584f00f5ddbb71938a2576cd84d
artaficialAI <tpieschala17@students.polytechnic.org>
2016-07-18 10:05:53 -0400

    Sanitized Input Array
    Added sanitizeArray method that converts doubles [-1,1] into [0-255]
    ints useful for RGB values

--- project_MasterCopy.py
+++ project_MasterCopy.py
@@ -16,8 +16,17 @@ class noise_Gen2D:
                 newNoise = noise.snoise2(x/(self.scale*self.resolution),y/(self.scale*self.resolution),self.octaves,self.persistence,self.lacunarity)
                 self.noiseList2D[x].append(newNoise)
         return self.noiseList2D
+    def sanatisePointList(self,pntArray):
+        for x in range(len(pntArray)):
+            for y in range(len(pntArray)):
+                pnt = pntArray[x][y]
+                pnt *= 127
+                pnt += 128
+                pntArray[x][y] = int(pnt)
+        return pntArray           
     
-Alpha = noise_Gen2D(1,10,3)
+Alpha = noise_Gen2D(10,10,1)
 Beta = Alpha.generate2DNoiseArray()
+Gamma = Alpha.sanatisePointList(Beta)
 for pos in range(Alpha.scale*Alpha.resolution):
-    print(Beta[pos])
+    print(Gamma[pos])
